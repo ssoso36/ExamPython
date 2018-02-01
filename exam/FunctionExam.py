1023419f867b62c7892361c10b26fb12bea80d80
김소연 <soyeon_k@encar.com>
2018-02-01 10:53:30 +0900

    오늘의 공부 2018.02.01

--- exam/FunctionExam.py
+++ exam/FunctionExam.py
@@ -30,5 +30,35 @@ c = functionTmp(10)
 print(c)
 
 
+def manyFunc(choice, *args):
+
+    if choice == "sum":
+        result = 0
+        for i in args:
+            result = result + i
+    elif choice == "multi":
+        result = 1
+        for i in args:
+            result = result * i
+    else:
+        result = "else....."
+    return result, choice, args
+
+print(manyFunc("sum", 1, 2, 3))
+# (6, 'sum', (1, 2, 3))
+print(manyFunc("multi", 1, 2, 3, 4, 5))
+# (120, 'multi', (1, 2, 3, 4, 5))
+
+
+def say_myself(name, old, man=True):
+    result = "이름 : %s, 나이 : %d, 성별 : " %(name, old)
+
+    if man:
+        result = result + "남자"
+    else:
+        result = result + "여자"
+    return result
+
+print(say_myself("test", 10))
 
 
